ffe8473b0901ee1b9c7f5b549828009cfed9c51c
Gursevak56 <gursevaksinghgill21@gmail.com>
2023-05-22 18:21:59 +0530

    initial commit

--- server.js
+++ server.js
@@ -1,22 +1,38 @@
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-dotenv.config();
-const app = require('./app.js');
-console.log(process.env.DB_CONN_STRING);
-const url = 'mongodb+srv://Gursevak:Gursevak%40123@cluster0.uhsysao.mongodb.net/clineFlex?retryWrites=true&w=majority';
-mongoose.connect(process.env.DB_CONN_STRING, {
-    useNewUrlParser: true
-}).then((conn) => {
-    console.log("DB Connected");
-}).catch(err=>{
-    console.log(err);
-})
-const port = process.env.PORT;
-app.listen(port, (err, value) => {
-    if (err) {
-        console.log(`app is not listen on port ${port}`);
-    }
-    else {
-        console.log(`app is listen on port ${port}`);
-    }
-})
+const express = require("express");
+const app = express();
+const fs = require("fs");
+const movieRouter = require("./Routes/moviesRouter.js");
+const usersrouter = require('./Routes/usersrouter.js');
+const errorHandler = require("./utils/errorHandler.js");
+const globalHandler = require("./globalError/globalHandler.js");
+const bodyparser = require("body-parser");
+app.use(bodyparser.urlencoded({ extended: true, limit: "50mb" }));
+app.use(bodyparser.json());
+app.use(express.json());
+app.use(express.static("./public"));
+app.use("/api/v1/movies", movieRouter);
+app.use('/api/v1/user', usersrouter);
+app.all("*", (req, res, next) => {
+  // res.status(404).json({
+  //     status:"fail",
+  //     message:`can't find url ${req.originalUrl} on the server`
+  // })
+  // const err=new Error(`cant't find this url ${req.originalUrl} on server`,404);
+  // err.statusCode=600;
+  // err.status="fail"
+  // next(err);
+
+  // app.use((err,req,res,next)=>{
+  //   err.statusCode=err.statusCode || 500;
+  //   err.status=err.status || 'fail';
+  //   res.status(err.statusCode).json({
+  //     status:err.status,
+  //     message:err.message
+  //   })
+  // })
+  const err = new errorHandler(`cant't find this url ${req.originalUrl}`, 415);
+  next(err);
+  app.use(globalHandler); //errorHandler middleware
+});
+
+module.exports = app;
